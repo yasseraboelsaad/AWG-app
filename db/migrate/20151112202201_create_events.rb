d32249aa955dfdbdd5e99b3856da5aef0e043e0b
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2015-11-12 22:24:43 +0200

    created event model

---
+++ db/migrate/20151112202201_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.string :committee
+      t.string :place
+      t.string :creator
+      t.integer :privacy
+      t.datetime :time
+      t.datetime :endtime
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151112202201) do
 
-  create_table "Users", force: :cascade do |t|
-    t.text "Fname",    null: false
-    t.text "Lname",    null: false
-    t.text "email",    null: false
-    t.text "password", null: false
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "committee"
+    t.string   "place"
+    t.string   "creator"
+    t.integer  "privacy"
+    t.datetime "time"
+    t.datetime "endtime"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
