318a8c0d846892fa395a8889a580a8771c99d933
Karim Farid <karim.f.1995@gmail.com>
2015-11-12 22:26:44 +0200

    User
    Creating users devise including all user authentication and tawabe3ha.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151112202448) do
 
-  create_table "Users", force: :cascade do |t|
-    t.text "Fname",    null: false
-    t.text "Lname",    null: false
-    t.text "email",    null: false
-    t.text "password", null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "fname"
+    t.string   "lname"
+    t.date     "dateOfBirth"
+    t.string   "country"
+    t.string   "city"
+    t.string   "gender"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
