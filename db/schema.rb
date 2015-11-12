3c3dacecac828b9d5e1a1fbe4b6d2601b5f8f793
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2015-11-12 22:43:51 +0200

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151112203216) do
 
   create_table "committees", force: :cascade do |t|
@@ -20,9 +19,6 @@ ActiveRecord::Schema.define(version: 20151112203216) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
-
-ActiveRecord::Schema.define(version: 20151112202448) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
