722231e127bf9b66474a55d84805311e3d53c862
Karim Farid <karim.f.1995@gmail.com>
2015-11-13 00:07:49 +0200

    Added has many to users
    users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :sent_messages, :class_name => 'Message', :foreign_key => 'sender_id'
-  has_many :recieved_messages, :class_name => 'Message', :foreign_key => 'reciever_id'
-
+	has_many :sent_messages, :class_name => 'Message', :foreign_key => 'sender_id'
+	has_many :recieved_messages, :class_name => 'Message', :foreign_key => 'reciever_id'
+	has_many :assigned_task, :class_name => 'Task', :foreign_key => 'assigner_id'
+	has_many :assigned_to_task, :class_name => 'Task', :foreign_key => 'assignee_id
 end
