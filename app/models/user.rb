e4130f4477bfe25ce974b8538059ee5ce470c2c4
Doola <ahmed.adel.abodeif@gmail.com>
2015-11-14 01:01:56 +0200

    Revert "Revert "fixing migrations""
    This reverts commit 2b3b7f759e4fd215aa97487f3c82a5fbd6b919db.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :sent_messages, :class_name => 'Message', :foreign_key => 'sender_id'
   has_many :recieved_messages, :class_name => 'Message', :foreign_key => 'reciever_id'
-
+  has_many :has_tasks_assigned, :class_name => 'Task', :foreign_key => 'assignee_id'
+  has_many :assigned_tasks_to, :class_name => 'Task', :foreign_key => 'assigner_id'
+  has_many :host, :class_name => 'Event', :foreign_key => 'user_id'
 end
--- db/migrate/20151112210452_create_messages.rb
+++ db/migrate/20151112210452_create_messages.rb
@@ -2,9 +2,9 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.text :content
-      t.foreign_key :sender_id 
       t.integer :sender_id
       t.integer :reciever_id
+      t.foreign_key :sender_id 
       t.foreign_key :reciever_id
       t.timestamps null: false
     end
