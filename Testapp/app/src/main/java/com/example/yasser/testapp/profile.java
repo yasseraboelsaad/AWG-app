1087c0e16610bf3a433c900d6e6bde3e9309ef15
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2015-11-28 09:55:11 +0200

    Revert "Merge remote-tracking branch 'origin/test-app' into test-app"
    This reverts commit 8042d594d8bfbf52f5a0c28f86d7193e5f9acc73, reversing
    changes made to 1fa35a57077de90685894ac0ca2829a2d264e238.

--- Testapp/app/src/main/java/com/example/yasser/testapp/profile.java
+++ Testapp/app/src/main/java/com/example/yasser/testapp/profile.java
@@ -3,13 +3,10 @@ package com.example.yasser.testapp;
 import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import java.lang.reflect.Array;
@@ -17,14 +14,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class profile extends Fragment implements View.OnClickListener {
+public class profile extends Fragment {
 
-    public String passable;
-    Button post;
-    EditText postBox;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
 
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
@@ -47,42 +40,7 @@ public class profile extends Fragment implements View.OnClickListener {
         lv.setAdapter(myarrayAdapter);
         myarrayAdapter.notifyDataSetChanged();
 
-       // post = (Button) this.getActivity().findViewById(R.id.postBtn);
-        //post.setOnClickListener(this);
-
-        //postBox = (EditText)this.getView().findViewById(R.id.postBox);
-
         return rootView;
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        post = (Button) this.getActivity().findViewById(R.id.postBtn);
-        post.setOnClickListener(this);
-
-        postBox = (EditText)this.getView().findViewById(R.id.postBox);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.postBtn)
-        {
-            Bundle bundle = this.getArguments();
-            ArrayList<String> post = bundle.getStringArrayList("posts");
-            if(!postBox.getText().toString().equals("Write a Post!"))
-            {
-                post.add(postBox.getText().toString());
-            }
-
-            // need to append the new post to the end of the list
-            MainActivity.setPosts(post);
-
-           /* for(int i=0; i<post.size();i++)
-            {
-                Log.v("aho aho aho = ",post.get(i));
-            }*/
-        }
-    }
 }
