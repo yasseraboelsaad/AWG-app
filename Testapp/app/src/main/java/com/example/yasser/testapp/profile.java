6792d55fae43d94ba49896a01c2f3c02aa8e60f3
Doola <ahmed.adel.abodeif@gmail.com>
2015-11-28 05:46:14 +0200

    changing profile structure
    - Decreasing avatar size
    - Changing Sample static info

--- Testapp/app/src/main/java/com/example/yasser/testapp/profile.java
+++ Testapp/app/src/main/java/com/example/yasser/testapp/profile.java
@@ -9,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,9 +21,23 @@ public class profile extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        String[] data = {
+                "First Name: Batman",
+                "Last Name: - ",
+                "email: batman@batcave.com",
+                "Country: USA",
+                "City: Gotham",
+                "Gender: Male"
+        };
+        ArrayList<String >userData = new ArrayList<String>(Arrays.asList(data));
+        //ListView lv = (ListView)findViewById(R.id.list);
+        //ArrayAdapter<String> myarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, userData);
+        //lv.setAdapter(myarrayAdapter);
+        //lv.setTextFilterEnabled(true);
+
         ListView lv = (ListView)rootView.findViewById(R.id.list);
-        List<String> myList = Arrays.asList("First Name", "Last Name", "email", "Country", "City", "Gender");
-        ArrayAdapter<String> myarrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, myList);
+        //List<String> myList = Arrays.asList("First Name", "Last Name", "email", "Country", "City", "Gender");
+        ArrayAdapter<String> myarrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, userData);
         lv.setAdapter(myarrayAdapter);
         lv.setTextFilterEnabled(true);
         lv.setAdapter(myarrayAdapter);
