8cfd148a8c8764fe4ef7e54bb170ab27971de72e
Doola <ahmed.adel.abodeif@gmail.com>
2015-12-12 22:45:16 +0200

    restoring rails app

--- app/models/task.rb
+++
@@ -1,4 +0,0 @@
-class Task < ActiveRecord::Base
-	belongs_to :assignee , :class_name => 'User' , :foreign_key => 'assignee_id'
-	belongs_to :assigner , :class_name => 'User' , :foreign_key => 'assigner_id'
-end
